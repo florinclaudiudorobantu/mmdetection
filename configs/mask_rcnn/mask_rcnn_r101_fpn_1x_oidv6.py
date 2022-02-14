c1cf2b41914aa90f356eafd9ebeea37408582418
Claudiu <doroptg@gmail.com>
2022-02-15 01:30:30 +0200

    oidv6_resnet50_maskrcnn

---
+++ configs/mask_rcnn/mask_rcnn_r101_fpn_1x_oidv6.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/mask_rcnn_r50_fpn_det_oidv6.py',
+    '../_base_/datasets/oidv6_detection.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
