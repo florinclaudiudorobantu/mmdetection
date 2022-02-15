f2a3d0cdd8f33bc2c15dfcabaa761d5f10642c62
Claudiu <doroptg@gmail.com>
2022-02-16 00:03:03 +0200

    update

--- configs/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_det_oidv6.py
+++ configs/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_det_oidv6.py
@@ -34,7 +34,7 @@ img_norm_cfg = dict(
 # augmentation strategy originates from DETR / Sparse RCNN
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='LoadAnnotations', with_bbox=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(
         type='AutoAugment',
@@ -71,7 +71,7 @@ train_pipeline = [
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
 data = dict(train=dict(pipeline=train_pipeline))
 
