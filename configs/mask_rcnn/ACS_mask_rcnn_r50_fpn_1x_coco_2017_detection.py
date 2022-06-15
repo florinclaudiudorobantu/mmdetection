bacd4c0468d61e01bd4d08503653fdeece63ce62
Claudiu <doroptg@gmail.com>
2022-06-15 18:47:58 +0300

    COCO 2017 configs

---
+++ configs/mask_rcnn/ACS_mask_rcnn_r50_fpn_1x_coco_2017_detection.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/ACS_mask_rcnn_r50_fpn_coco_2017_detection.py',
+    '../_base_/datasets/ACS_coco_2017_detection.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
